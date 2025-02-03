bda9b14dec25fe8cb4c59c682d1eed925debe9ba
XAILOX1337 <olgklm07@gmail.com>
2025-02-03 16:09:30 +0300

    Some edit

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include "mainwindow.h"
-
+//some new lines
 #include <QApplication>
 
 int main(int argc, char *argv[]){
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -11,7 +11,7 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
 }
-
+//chipi chapa
 QVector<int> Stations;
 void MainWindow::mouseReleaseEvent(QMouseEvent *event)
 {
